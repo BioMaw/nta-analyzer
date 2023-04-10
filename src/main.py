88479aa422a12b011d0efc6eb4b30613e75a9744
BioMaw <martint.maw@gmail.com>
2023-04-10 22:00:18 +0200

    first stable version

--- src/main.py
+++ src/main.py
@@ -13,6 +13,8 @@ import pandas as pd
 from pathlib import Path
 import seaborn as sns
 import matplotlib.pyplot as plt
+import openpyxl
+
 
 def parse_file(file):
     #read headerless files
@@ -30,6 +32,7 @@ def parse_file(file):
     df['biol_replic']=part_list[3]
     df['treatment']=part_list[4]
     df['dilution']=int(part_list[5][3:])
+    #print('dilution for file' + str(file.stem) + ' = ' + str(int(part_list[5][3:]))) 
     df['tech_replic']=int(part_list[8])
 
     # calculate additional columns
@@ -37,11 +40,47 @@ def parse_file(file):
     # drop redundant columns
     df = df.drop(columns=['Number', 'Volume / nm^3','Area / nm^2','Concentration / cm-3'])
     return df
+
+def plot_hist(df, binwidth = 5, export = False):
+    #plot histograms:
+    # Get list of unique cell lines
+    cell_lines = df['biol_replic'].unique()
+
+    # Create subplot grid
+    fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(10, 8))
+
+    for i, cell_line in enumerate(cell_lines):
+        row = i // 2
+        col = i % 2
+        ax = axes[row, col]
+
+        p_data = df.loc[df['biol_replic'] == cell_line]
+        sns.histplot(data=p_data, x='Size / nm', weights='conc_corr_norm', 
+                    hue='treatment',  binwidth=binwidth, ax=ax)
+        ax.set_title('Cell line {}'.format(cell_line))
+
+    # Add overall title and axis labels
+    figtitle='Histograms by cell line and treatment. Binsize = ' + str(binwidth) + ' nm'
+    plt.suptitle(figtitle)
+    fig.text(0.5, 0.04, 'Size / nm', ha='center')
+    fig.text(0.04, 0.5, 'Probability Mass Function', va='center', rotation='vertical')
+
+    # Adjust spacing between plots
+    plt.tight_layout()
+
+    # Show plot
+    plt.show()
+    if export:
+       fig.savefig(Path(export, figtitle + ' .png'), format='png', dpi=300, bbox_inches='tight') 
+
 INPUT_DIR = Path('/mnt/c/Projects/Data/ev_sizes')
 FIRST_LINE =74
 # generate folder for headerless files
 NO_HEAD_DIR = Path(INPUT_DIR, 'no_head')
 NO_HEAD_DIR.mkdir(exist_ok=True)
+
+OUTPUT_DIR = Path(INPUT_DIR, 'exports')
+OUTPUT_DIR.mkdir(exist_ok=True)
 
 file_list= list(INPUT_DIR.glob('*.txt'))
 df_master = pd.DataFrame()
@@ -62,6 +101,9 @@ df_master.reset_index(drop=True, inplace=True)
 #clean up some errors in User Input, make all treatment lower case
 df_master['treatment']=df_master['treatment'].str.lower()
 
+# remove 200 dilution data
+df_master = df_master.loc[df_master['dilution'] != 200]
+
 ### start dataset manipulation
 # truncate based on size
 # cut off above the highest occuring size
@@ -73,18 +115,22 @@ df_master=df_master.loc[df_master['Size / nm'] < max_valid_size]
 min_cutoff = 30
 df_master=df_master.loc[df_master['Size / nm'] > min_cutoff]
 
-# add classification based on size
-is_small_class = (df_master['Size / nm'] > 30) & (df_master['Size / nm'] <= 150) 
-is_large_class = (df_master['Size / nm'] >= 151) & (df_master['Size / nm'] <= 800)
-df_master.loc[is_small_class, 'Size_Class'] = 'small_class'
-df_master.loc[is_large_class, 'Size_Class'] = 'large_class'
-
 # normalize each technical replicate by their own total concentration
 # get sum for each treatment of each tech_replic of each biol_replic
 sum_conc = df_master.groupby(['biol_replic', 'tech_replic', 'treatment'])['conc_corr'].transform('sum')
 
 # normalization to 1
+df_master['sum'] = sum_conc
 df_master['conc_corr_norm'] = df_master['conc_corr'] / sum_conc
+
+### check if number of nonzero sizes is equal for all treatments and replics
+def count_unique_nonzero(x):
+    return x[x != 0].nunique()
+
+# Apply custom function to grouped DataFrame
+unique_sizes = df_master.groupby(['biol_replic', 'tech_replic', 'treatment'])['conc_corr'].apply(count_unique_nonzero)
+# Reset index to turn resulting Series into a DataFrame
+unique_concentrations = unique_sizes.reset_index()
 
 ## averaging over technical replic
 df_avg = df_master.groupby(['biol_replic', 'treatment', 'Size / nm']).mean()
@@ -99,34 +145,24 @@ mean_df = df_avg.groupby(['biol_replic', 'treatment'])[['Size / nm', 'conc_corr_
 mean_df = mean_df.reset_index()
 mean_df.columns = ['biol_replic', 'treatment', 'Mean']
 
-# plot histograms:
-# Get list of unique cell lines
-cell_lines = df_avg['biol_replic'].unique()
+# add classification based on size
+is_small_class = (df_avg['Size / nm'] > 30) & (df_avg['Size / nm'] <= 150) 
+is_large_class = (df_avg['Size / nm'] >= 151) & (df_avg['Size / nm'] <= 800)
+df_avg.loc[is_small_class, 'Size_Class'] = 'small_class'
+df_avg.loc[is_large_class, 'Size_Class'] = 'large_class'
 
-# Create subplot grid
-fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(10, 8))
+plot_hist(df_avg, binwidth=2.5, export = OUTPUT_DIR)
+plot_hist(df_avg, binwidth=5, export = OUTPUT_DIR)
+plot_hist(df_avg, binwidth=10, export = OUTPUT_DIR)
+plot_hist(df_avg, binwidth=20, export = OUTPUT_DIR)
+plot_hist(df_avg, binwidth=50, export = OUTPUT_DIR)
+plot_hist(df_avg, binwidth=120, export = OUTPUT_DIR)
 
-for i, cell_line in enumerate(cell_lines):
-    row = i // 2
-    col = i % 2
-    ax = axes[row, col]
+#export table to excel
+df_avg.to_excel(Path(OUTPUT_DIR, 'data_long_table.xlsx'))
 
-    p_data = df_avg.loc[df_avg['biol_replic'] == cell_line]
-    sns.histplot(data=p_data, x='Size / nm', weights='conc_corr_norm', hue='treatment', multiple='stack', binwidth=5, ax=ax)
-    ax.set_title('Cell line {}'.format(cell_line))
-
-# Add overall title and axis labels
-plt.suptitle('Histograms by cell line and treatment')
-fig.text(0.5, 0.04, 'Size / nm', ha='center')
-fig.text(0.04, 0.5, 'Count', va='center', rotation='vertical')
-
-# Adjust spacing between plots
-plt.tight_layout()
-
-# Show plot
-plt.show()
-
-
+#export mean results to excel
+mean_df.to_excel(Path(OUTPUT_DIR, 'mean_dist_result.xlsx'))
 # means = df_avg.groupby(['biol_replic', 'treatment']).mean()
 # mean_value = (df['Size / nm'] * df['conc_corr_norm']).sum() / df['Count'].sum()
 # ## violinplot improvement needed
